da6a3972b89e79dc4e837275c06740f7b422ede5
Johnny <strikax@gmail.com>
2018-12-10 18:26:58 +0000

    Adds admin features.

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from common.models import *
 
 # Register your models here.
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('value', 'expires', 'enabled')
+    list_filter = ('enabled', 'created', 'updated', 'expires')
+    
+class HostAdmin(admin.ModelAdmin):
+    list_display = ('fqdn', 'hostname', 'domain', 'created', 'enabled')
+    list_filter = ('enabled', 'created', 'updated')
+
+admin.site.register(Token, TokenAdmin)
+admin.site.register(Host, HostAdmin)
+admin.site.register(User)
