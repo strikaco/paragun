d47a3dc019b7805facb8cc46c445e9d171690921
Johnny <strikax@gmail.com>
2019-02-28 18:23:52 +0000

    Implements basic parsing framework.

--- parsing/admin.py
+++ parsing/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from parsing.models import *
 
 # Register your models here.
+class AssertionInline(admin.TabularInline):
+    model = Assertion
+    fields = ('key', 'value', 'enabled')
+    extra = 0
+    
+
+class ParserInline(admin.TabularInline):
+    model = Parser
+    fields = ('field', 'priority', 'value', 'benchmark', 'enabled')
+    extra = 0
+    
+    
+class SampleInline(admin.TabularInline):
+    model = Sample
+    fields = ('created', 'value', 'status')
+    extra = 0
+    
+    
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('key', 'description', 'enabled')
+    list_filter = ('enabled', 'created', 'updated')
+    search_fields = ('key', 'description', 'tags')
+    
+    
+class ParserAdmin(admin.ModelAdmin):
+    list_display = ('service', 'field', 'priority', 'value', 'benchmark', 'enabled')
+    list_filter = ('enabled', 'created', 'updated', 'service__key', 'field')
+    
+  
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('service', 'value', 'status', 'enabled')
+    list_filter = ('enabled', 'created', 'updated', 'status', 'service__key')
+  
+    inlines = [
+        AssertionInline,  
+    ]
+    
+  
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = [
+        ParserInline,
+        SampleInline,
+    ]
+    
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Parser, ParserAdmin)
+admin.site.register(Sample, SampleAdmin)
+admin.site.register(Field, FieldAdmin)
--- parsing/tests.py
+++ parsing/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from parsing.models import *
 
 # Create your tests here.
+class ParsingTest(TestCase):
+    
+    def setUp(self):
+        # Create a service
+        self.service = Service.objects.create(key='ssh')
+        
+        # Create some samples and assertions
+        samples = (
+            {'raw': 'Aug  1 18:27:46 knight sshd[20325]: Failed password for illegal user test from 218.49.183.17 port 48849 ssh2', 'user': 'test', 'src_ip': '218.49.183.17'},
+        )
+        
+        for sample in samples:
+            obj = Sample.objects.create(service=self.service, value=sample.pop('raw'))
+            for k,v in sample.items():
+                field = Field.objects.get_or_create(key=k)[0]
+                obj.assertions.create(key=field, value=v)
+        
+        # Create some parsers
+        parsers = (
+            {'key': 'src_ip', 'value': 'from ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'},
+            {'key': 'user', 'value': 'user ([a-zA-Z0-9\.\-]+) from'},
+        )
+        
+        for parser in parsers:
+            field = Field.objects.get_or_create(key=parser['key'])[0]
+            Parser.objects.create(service=self.service, enabled=True, field=field, value=parser['value'])
+            
+    def test_service_validation(self):
+        self.service.test()
